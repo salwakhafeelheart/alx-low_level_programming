dc27cc8b73f562e2852b7741c0b614b2836cff43
salwakhafeelheart <salwaolonade@gmail.com>
2022-06-07 14:46:33 -0700

    function0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#include "holberton.h"
-/**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
- */
- int main(void)
-{
-	char *sh = "Holberton";
+#include "main.h"
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+/**
+ *main - Entry point
+ *
+ *Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	putchar('');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
