7de4ca73d9aedc05293a535d3e9405905329cdc4
salwakhafeelheart <salwaolonade@gmail.com>
2022-06-27 00:37:17 -0700

    commiting task5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  *argstostr - prints args
  *@ac: takes in width of grid
  *@av: height of grid
+ *
  *Return: the args one line at a time
  */
-
 char *argstostr(int ac, char **av)
 {
 	char *str;
@@ -13,9 +14,11 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
 	while (a < ac)
 	{
 		b = 0;
+
 		while (av[a][b] != '\0')
 		{
 			count++;
@@ -25,12 +28,15 @@ char *argstostr(int ac, char **av)
 	}
 	count = count + ac + 1;
 	str = malloc(sizeof(char) * count);
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
+
 	for (a = 0; a < ac; a++)
 	{
+
 		for (b = 0; av[a][b] != '\0'; b++)
 		{
 			str[c] = av[a][b];
